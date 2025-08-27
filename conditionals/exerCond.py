fb2b8287c28c0dc6c5488de5bf8405125ef36ccb
Wilma Aura <mornlitoralis@gmail.com>
2025-08-27 22:41:31 +0700

    add exercise

---
+++ conditionals/exerCond.py
@@ -0,0 +1,19 @@
+age = int(input("Enter your age:"))
+
+if age >= 18:
+    print("You are old enough to learn to drive")
+else:
+    print("You need more", 18-age, "to learn to drive")
+
+my_age = 20
+
+if age > my_age:
+    print("You are", abs(age-my_age), "older then me")
+else:
+    print("You are", abs(age-my_age), "younger then me")
+
+
+
+
+
+
