5f530fc7f444c1bdba907b3948adc78f978497d6
wilma <mornlitoralis@gmail.com>
2025-12-21 21:29:18 +0700

    add file

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+memo = {}
+
+def fibonacci(n):
+    if n in memo:
+        return memo [n]
+    
+    if n <=1:
+        return n
+    
+    memo [n] = fibonacci(n-1) + fibonacci(n-2)
+    return memo[n]
+
+result = fibonacci(10)
+print('Fibonacci Sequence:', result)
